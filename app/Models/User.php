4a72630ca3cfbabd0f161497d539263c5164aefa
thomas <thomas.zeryouh@goons.fr>
2026-05-22 13:25:09 +0200

    act test correction

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,9 @@ class User extends Authenticatable
     }
 
     public function usesProfessionalEmail(): bool
-{
-    return !preg_match('/@(gmail|yahoo|hotmail|live|outlook)\./', $this->email);
-}
+    {
+
+        return !preg_match('/@(gmail|yahoo|hotmail|live|outlook)\./', $this->email);
+        
+    }
 }
